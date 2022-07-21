7cc61132a0fe642cdef57f7fc23791f7270ee7bc
Bruno Ferreira <bgsf1991@gmail.com>
2022-07-21 18:09:59 -0300

    Resposta dos Exercícios com Operadores

---
+++ exercicio3/script.js
@@ -0,0 +1,24 @@
+////////////////// EXERCÍCIO NÚMERO 3 - TRUE OR FALSE ///////////////////////
+console.log('   TRUE OR FALSE');
+console.log('');
+
+const operacao1 = 5 > 20 && 5 < 2;
+console.log('a) 5 é maior que 20 e também é menor que 2:', operacao1);
+const operacao2 = 5 === 5 && 5 === '5';
+console.log('b) 5 é igual a 5 ou é igual a “5”:', operacao2);
+const operacao3 = !20 > 50;
+console.log('c) negação de vinte é maior que cinquenta:', operacao3);
+const operacao4 = !(20 > 50);
+console.log('d) negação de (vinte é maior que cinquenta):', operacao4);
+
+
+
+// Escreva as operações lógicas abaixo em formato de código. Imprima no console se o valor da operação é verdadeira ou falsa:
+
+// a) 5 é maior que 20 e também é menor que 2;
+
+// b) 5 é igual a 5 ou é igual a “5”;
+
+// c) negação de vinte é maior que cinquenta
+
+// d) negação de (vinte é maior que cinquenta)
