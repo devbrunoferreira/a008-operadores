dccf3c4339160cb1b8d804e2bae6a29107e00aa3
Bruno Ferreira <bgsf1991@gmail.com>
2022-07-21 20:54:27 -0300

    Pequenas alterações nos Exercícios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -2,14 +2,17 @@
 console.log('NÚMERO PAR');
 console.log('');
 
+
 let numero, resto;
+
 numero = Number(prompt('Digite um número par:'));
 while (numero % 2 === 1) {
     numero = Number(prompt(`${numero} não é par, escolha outro número:`));
 }
+
 console.log('O número escolhido foi:', numero);
 resto = numero % 2;
-console.log('O resto da divisão é:', resto);
+console.log('O resto da divisão por 2 é:', resto);
 
 // C) Por se tratar de números pares o valor retornado é sempre 0 (ZERO);
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -4,10 +4,13 @@ console.log('');
 
 const operacao1 = 5 > 20 && 5 < 2;
 console.log('a) 5 é maior que 20 e também é menor que 2:', operacao1);
-const operacao2 = 5 === 5 && 5 === '5';
+
+const operacao2 = 5 === 5 || 5 === '5';
 console.log('b) 5 é igual a 5 ou é igual a “5”:', operacao2);
+
 const operacao3 = !20 > 50;
 console.log('c) negação de vinte é maior que cinquenta:', operacao3);
+
 const operacao4 = !(20 > 50);
 console.log('d) negação de (vinte é maior que cinquenta):', operacao4);
 
